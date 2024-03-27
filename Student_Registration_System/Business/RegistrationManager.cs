7dc54196dd4b1c666a71baae79639ccfcd17f589
Bilal2425 <shaikebilal123@gmail.com>
2024-03-27 12:36:32 -0400

    Design update

---
+++ Student_Registration_System/Business/RegistrationManager.cs
@@ -0,0 +1,19 @@
+﻿using Student_Registration_System.Repository;
+
+namespace Student_Registration_System.Business
+{
+    public class RegistrationManager
+    {
+        private readonly RegistrationRepository _registrationRepository;
+
+        public RegistrationManager(RegistrationRepository registrationRepository)
+        {
+            _registrationRepository = registrationRepository;
+        }
+
+        public void RegisterStudent(string userName, string password)
+        {
+            _registrationRepository.RegisterStudent(userName, password);
+        }
+    }
+}
---
+++ Student_Registration_System/Repository/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Student_Registration_System.Models;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Student_Registration_System.Repository
+{
+    public class UserRepository
+    {
+        private readonly string _connectionString;
+
+        public UserRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("SqlServerConnection");
+        }
+
+        public User GetUser(string userName, string password)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM users WHERE username = @UserName AND password = @Password";
+
+                return connection.QueryFirstOrDefault<User>(query, new { UserName = userName, Password = password });
+            }
+        }
+    }
+}
